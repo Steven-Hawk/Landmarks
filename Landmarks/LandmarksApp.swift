c0407357d774d852f30971564f5b3c6d07fffccf
Steven Hawk <stevenhawk3@icloud.com>
2025-07-03 16:12:01 -0500

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Steven Hawk on 7/3/25.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
